df222c28e8a7d2f91b791d7b9f67f743a0091563
Salim Zepcom <salim.zepcom@gmail.com>
2023-06-09 00:41:30 +0500

    Update migration code

--- database/migrations/2023_06_08_193834_create_files_table.php
+++ database/migrations/2023_06_08_193834_create_files_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('path');
             $table->timestamps();
         });
     }
